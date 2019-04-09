5e12a402b17192afb3004c041efb2e68628b19e5
kouwq <kouwq@smail.swufe.edu.cn>
2019-04-09 20:40:03 +0800

    4/9 config and menu

---
+++ app/src/main/java/com/demo/hello/kwq/ConfigActivity.java
@@ -0,0 +1,61 @@
+package com.demo.hello.kwq;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class ConfigActivity extends AppCompatActivity {
+    public final String TAG="ConfigActivity";
+    EditText dollarText;
+    EditText euroText;
+    EditText wonText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+        Intent intent= getIntent();
+        float dollar2= intent.getFloatExtra("dollar_rate_key",0.0f);
+        float euro2= intent.getFloatExtra("euro_rate_key",0.0f);
+        float won2= intent.getFloatExtra("won_rate_key",0.0f);
+
+        Log.i(TAG,"OnCreate:dollar2"+dollar2);
+        Log.i(TAG,"OnCreate:euro2"+euro2);
+        Log.i(TAG,"OnCreate:won2"+won2);
+
+        dollarText =findViewById(R.id.dollar_rate);
+        euroText =findViewById(R.id.euro_rate);
+        wonText =findViewById(R.id.won_rate);
+
+        dollarText.setText(String.format("%.2f",dollar2));
+        euroText.setText(String.format("%.2f",euro2));
+        wonText.setText(String.format("%.2f",won2));
+
+    }
+
+    public void save(View btn){
+        Log.i(TAG,"save:");
+        //获取新值
+        float newDollar=Float.parseFloat(dollarText.getText().toString());
+        float newEuro=Float.parseFloat(euroText.getText().toString());
+        float newWon=Float.parseFloat(wonText.getText().toString());
+        Log.i(TAG,"save:dollar2"+newDollar);
+        Log.i(TAG,"save:euro2"+newEuro);
+        Log.i(TAG,"save:won2"+newWon);
+        //保存到Bundle或放入Extra
+        Intent intent = getIntent();
+        Bundle bdl=new Bundle();
+        bdl.putFloat("key_dollar",newDollar);
+        bdl.putFloat("key_euro",newEuro);
+        bdl.putFloat("key_won",newWon);
+        intent.putExtras(bdl);
+        setResult(2,intent);
+        //返回调用页面
+        finish();
+    }
+
+
+}
--- app/src/main/java/com/demo/hello/kwq/RateActivity.java
+++ app/src/main/java/com/demo/hello/kwq/RateActivity.java
@@ -1,20 +1,25 @@
 package com.demo.hello.kwq;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DecimalFormat;
-
 public class RateActivity extends AppCompatActivity {
+    public final String TAG="RateActivity";
+
     EditText rmb;
     TextView show;
+    float dollarRate = 1/6.7f;
+    float euroRate = 1/11f;
+    float wonRate = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,24 +43,73 @@ public class RateActivity extends AppCompatActivity {
         }
 
         if (btn.getId() == R.id.btn_dollar) {
-            val = r * (1/6.7f);
+            val = r * dollarRate;
         } else if (btn.getId() == R.id.btn_euro) {
-            val = r * (1/11f);
+            val = r * euroRate;
         } else {
-            val = r * 500;
+            val = r * wonRate;
         }
-        DecimalFormat df=new DecimalFormat("#.00");
-        show.setText(String.valueOf(df.format(val)));
+        show.setText(String.format("%.2f",val));
+//        DecimalFormat df=new DecimalFormat("#.00");
+//        show.setText(String.valueOf(df.format(val)));
 //        show.setText("" + val);
+    }
+
+    private void openConfig() {
+        Intent config=new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG,"openOne:dollarRate="+dollarRate);
+        Log.i(TAG,"openOne:euroRate="+euroRate);
+        Log.i(TAG,"openOne:wonRate="+wonRate);
+
+        startActivityForResult(config,1);
     }
 
     public void openOne(View v){
         //打开一个Activity
-        Log.i("open","Open One page:");
-        Intent hello=new Intent(this,SecActivity.class);
-        Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.baidu.com/"));
-        Intent call = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:13550585364"));
-        startActivity(call);
+//        Log.i("open","Open One page:");
+//        Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.baidu.com/"));
+//        Intent call = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:13550585364"));
+//        startActivity(call);
+//        finish();
+        openConfig();
+//        startActivity(config);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1&&resultCode==2){
+//            bdl.putFloat("key_dollar",newDollar);
+//            bdl.putFloat("key_euro",newEuro);
+//            bdl.putFloat("key_won",newWon);
+            Bundle bundle=data.getExtras();
+            dollarRate=bundle.getFloat("key_dollar",0.1f);
+            euroRate=bundle.getFloat("key_euro",0.1f);
+            wonRate=bundle.getFloat("key_won",0.1f);
+            Log.i(TAG,"onActivityResult:dollarRate="+dollarRate);
+            Log.i(TAG,"onActivityResult:euroRate="+euroRate);
+            Log.i(TAG,"onActivityResult:wonRate="+wonRate);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
